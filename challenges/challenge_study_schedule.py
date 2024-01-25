5ed49b0ad6754694dbf3d2af2cf374d39a18ea77
Filipe Marques Hottis <lipehottis@gmail.com>
2024-01-25 13:12:57 -0300

    feat: First Chalanger Complete - study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """BLZ :3"""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count_online_period = 0
+
+    for start, end in permanence_period:
+        if (
+            not isinstance(start, int)
+            or not isinstance(end, int)
+            or start > end
+        ):
+            return None
+
+        if start <= target_time <= end:
+            count_online_period += 1
+
+    return count_online_period
