b8af80d947b424f1fe0501f40f348ba51f3da4df
Filipe Marques Hottis <lipehottis@gmail.com>
2024-01-25 15:46:53 -0300

    feat: :sparkles: Challenge 5: Implement a Function to Check for Duplicate Numbers in a Tuple

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,32 @@
+def is_number_nagative_in_array(nums):
+    if (
+        nums is None
+        or len(nums) < 2
+    ):
+        return False
+
+    for num in nums:
+        if (
+            not isinstance(num, int)
+            or num < 0
+        ):
+            return True
+
+    return False
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        nums is None
+        or len(nums) < 2
+        or is_number_nagative_in_array(nums)
+    ):
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
